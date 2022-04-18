1cdab4584e3c1dda67d1941d80a13a76ac1a8879
Vladislav <91915428+UniverTime@users.noreply.github.com>
2022-04-19 00:03:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+
+#define DEBUG\
+    printf(__PRETTY_FUNCTION__);\
+    printf(" %c, %zu\n", WORKING_TAPE[IP], IP);
 
 size_t IP = 0;
 
 const char* WORKING_TAPE = nullptr;//рабочая лента
 
+/*
+
+G Grammar
+E Expression
+T Term
+D Degree
+P Primary expression
+N Number
+V Variable
+
+G::= E'$'
+E::= T{ [+-]T }*
+T::= D{ [*\]D }*
+D::= P{ [^]P }*
+P::= '('E')' | N | V
+V::= [a-z,A-Z]
+N::= [0-9]+
+
+
+*/
+
+//===================================================
+
 int getG(void);
 int getE(void);
 int getT(void);
+int getD(void);
 int getP(void);
 int getN(void);
+int getV(void);
+void gap_up(void);
 
 int main(void)
 {
@@ -30,6 +61,8 @@ int getG(void)
 
     int val = getE();
 
+    DEBUG
+
     if (WORKING_TAPE[IP] != '$')
     {
         printf("\n SYNTAX ERROR!!");
@@ -44,6 +77,8 @@ int getE(void)
 {
     int val = getT();
 
+    DEBUG
+
     while (WORKING_TAPE[IP] == '+' || WORKING_TAPE[IP] == '-')
     {
         if (WORKING_TAPE[IP++] == '+')
@@ -57,14 +92,80 @@ int getE(void)
     return val;
 }
 
+int getT(void)
+{
+    int val = getD();
+
+    DEBUG
+
+    while (WORKING_TAPE[IP] == '*' || WORKING_TAPE[IP] == '/')
+    {
+        if (WORKING_TAPE[IP++] == '*')
+        {
+            val *= getD();
+        }
+        else if (WORKING_TAPE[IP-1] == '/')
+        {
+            val /= getD();
+        }
+        else
+            val = (int) pow(val, getD());
+    }
+
+    return val;
+}
+
+int getD(void)
+{
+    int val = getP();
+
+    DEBUG
+
+    while (WORKING_TAPE[IP] == '^')
+    {
+            ++IP;
+
+            val = (int) pow(val, getP());
+    }
+
+    return val;
+}
+
+int getP(void)
+{
+    int val = 0;
+
+    DEBUG
+
+    if (WORKING_TAPE[IP] == '(')
+    {
+        ++IP;
+
+        val = getE();
+
+        if (WORKING_TAPE[IP] != ')')
+        {
+            printf("\n SYNTAX ERROR!!");
+
+            assert (0);
+        }
+        else
+            ++IP;
+    }
+    else if ('0' <= WORKING_TAPE[IP] && WORKING_TAPE[IP] <= '9')
+        val = getN();
+    else
+        val = getV();
+
+    return val;
+}
+
 int getN(void)
 {
     int val         =  0;
     size_t supIP    = IP;
 
-    printf("getN");
-
-    printf(" %c, %zu\n", WORKING_TAPE[IP], IP);
+    DEBUG
 
     while ('0' <= WORKING_TAPE[IP] && WORKING_TAPE[IP] <= '9')
     {
@@ -83,44 +184,35 @@ int getN(void)
     return val;
 }
 
-int getP(void)
+int getV(void)
 {
-    int val = 0;
+    int val         =  0;
+    size_t supIP    = IP;
 
-    if (WORKING_TAPE[IP] == '(')
+    DEBUG
+
+    if (('a' <= WORKING_TAPE[IP] && WORKING_TAPE[IP] <= 'z')
+        ||
+        ('A' <= WORKING_TAPE[IP] && WORKING_TAPE[IP] <= 'Z')
+       )
     {
+        val = 11;
+
         ++IP;
-
-        val = getE();
-
-        if (WORKING_TAPE[IP] != ')')
-        {
-            printf("\n SYNTAX ERROR!!");
-
-            assert (0);
-        }
-        else
-            ++IP;
     }
-    else
-        val = getN();
+
+    if (supIP == IP)
+    {
+        printf("\n SYNTAX ERROR!!");
+
+        assert (0);
+    }
 
     return val;
 }
 
-int getT(void)
+void gap_up()
 {
-    int val = getP();
-
-    while (WORKING_TAPE[IP] == '*' || WORKING_TAPE[IP] == '/')
-    {
-        if (WORKING_TAPE[IP++] == '*')
-        {
-            val *= getP();
-        }
-        else
-            val /= getP();
-    }
-
-    return val;
+    while (WORKING_TAPE[IP] == ' ' || WORKING_TAPE[IP] == '\n')
+        IP++;
 }
